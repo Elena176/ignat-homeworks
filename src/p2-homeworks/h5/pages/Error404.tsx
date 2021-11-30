a9e9c23bf3100b38259aa3f5fc517778a24b5c22
Elena176 <lena.tdn176@gmail.com>
2021-11-30 12:54:24 +0300

    added page 404

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import s from './Error404.module.css';
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
+        <div className={s.topBar}>
+            <div className={s.errorBlock}></div>
+            <div className={s.errorBlockText}>
+                <div className={s.errorText}>
+                    Oops, This page does not exist
+                </div>
+            </div>
+            {/*<div>404</div>
             <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>*/}
         </div>
     )
 }
--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -4,7 +4,6 @@ import Error404 from './Error404';
 function JuniorPlus() {
     return (
         <div>
-            <h2>This page is under development</h2>
             <Error404/>
             {/*<HW12/>*/}
         </div>
