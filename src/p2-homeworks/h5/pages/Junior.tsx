a6d6062267d5cf3072696657c850840bbca397a4
Elena176 <lena.tdn176@gmail.com>
2022-06-28 01:07:10 +0300

    added HW8

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Error404 from './Error404';
 import HW7 from '../../h7/HW7';
+import HW8 from '../../h8/HW8';
 
 function Junior() {
-    return (
-        <div>
-            {/*<Error404/>*/}
-             <HW7/>
-          {/*<HW8/>
-            <HW9/>
+  return (
+    <div>
+      {/*<Error404/>*/}
+      <HW7/>
+      <HW8/>
+      {/*<HW9/>
             <HW10/>*/}
-            {/*<HW11/>*/}
-        </div>
-    )
+      {/*<HW11/>*/}
+    </div>
+  )
 }
 
 export default Junior
