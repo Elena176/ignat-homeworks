5c74f13d10d266b5076e8340c7ea64e75a41315f
Elena176 <lena.tdn176@gmail.com>
2022-06-28 23:44:38 +0300

    added added HW9

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Error404 from './Error404';
 import HW7 from '../../h7/HW7';
 import HW8 from '../../h8/HW8';
+import HW9 from '../../h9/HW9';
 
 function Junior() {
   return (
@@ -9,8 +10,8 @@ function Junior() {
       {/*<Error404/>*/}
       <HW7/>
       <HW8/>
-      {/*<HW9/>
-            <HW10/>*/}
+      <HW9/>
+      {/*<HW10/>*/}
       {/*<HW11/>*/}
     </div>
   )
